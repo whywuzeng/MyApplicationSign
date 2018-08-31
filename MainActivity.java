42aea9845ccb60bd397088e5e44926e2093380e2
whywuzeng <jiushiqiangone@sina.com>
2018-08-31 11:43:21 +0800

    company 8-31

--- MainActivity.java
+++ MainActivity.java
@@ -1,14 +1,74 @@
 package com.example.wz1.mysigninapplication;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.example.wz1.mysigninapplication.adapter.MainAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
+    private MainAdapter mAdapter;
+    private List<String> mDatas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        initData();
+        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerview);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+        mRecyclerView.addItemDecoration(new DividerItemDecoration(this,DividerItemDecoration.VERTICAL));
+        mAdapter = new MainAdapter(this,mDatas);
+        mRecyclerView.setAdapter(mAdapter);
+
+        /**
+         * 既然是动画，就会有时间，我们把动画执行时间变大一点来看一看效果
+         */
+        DefaultItemAnimator defaultItemAnimator = new DefaultItemAnimator();
+        defaultItemAnimator.setAddDuration(1000);
+        defaultItemAnimator.setRemoveDuration(1000);
+        mRecyclerView.setItemAnimator(defaultItemAnimator);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    /**
+     * 这里通过两个菜单按钮来观看我们的动画效果
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case R.id.action_add:
+                mAdapter.addData(1);
+                break;
+            case R.id.action_remove:
+                mAdapter.removeData(mDatas.size()-1);
+                break;
+        }
+        return true;
+    }
+
+
+    private void initData() {
+        mDatas = new ArrayList<>();
+        for (int i = 'A'; i < 'Z'; i++) {
+            mDatas.add("" + (char) i);
+        }
     }
 }
